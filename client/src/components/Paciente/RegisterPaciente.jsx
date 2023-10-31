35cc49de9688a213aa9ff44dbe04a37de5b2b4ae
KeilaCruz <zS20019068@estudiantes.uv.mx>
2023-10-31 13:31:56 -0600

    Agregando estilos registrar paciente

--- client/src/components/Paciente/RegisterPaciente.jsx
+++ client/src/components/Paciente/RegisterPaciente.jsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form"
 import { addPaciente, getAllPacientes } from "../../api/Pacientes.api"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-
+import "../../css/styles.css"
 export function RegisterPaciente() {
     const { register, handleSubmit } = useForm()
     const [pacientes, setPacientes] = useState([])
@@ -56,30 +56,45 @@ export function RegisterPaciente() {
     }
     return (
         <>
+            <h3 className="title">FICHA DE IDENTIDAD DEL PACIENTE</h3>
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Nombre" {...register("nombre", { required: true })} />
-                <input type="text" placeholder="Apellido paterno" {...register("apePaterno", { required: true })} />
-                <input type="text" placeholder="Apellido materno" {...register("apeMaterno", { required: true })} />
-                <input type="number" placeholder="Edad" {...register("edad", { required: true })} />
-                <select name="estado_civil" {...register("estado_civil", { required: true })}>
+                <label className="label-form">Nombres(s)</label>
+                <input className="input-form" type="text" placeholder="Nombre" {...register("nombre", { required: true })} />
+                <label className="label-form">Apellido paterno</label>
+                <input className="input-form" type="text" placeholder="Apellido paterno" {...register("apePaterno", { required: true })} />
+                <label className="label-form">Apellido materno</label>
+                <input className="input-form" type="text" placeholder="Apellido materno" {...register("apeMaterno", { required: true })} />
+                <label className="label-form">Edad</label>
+                <input className="input-form" type="number" placeholder="Edad" {...register("edad", { required: true })} />
+                <label className="label-form">Estado civil</label>
+                <select className="input-form" name="estado_civil" {...register("estado_civil", { required: true })}>
                     <option value="Soltero">Soltero</option>
                     <option value="Casado">Casado</option>
                     <option value="Divorciado">Divorciado</option>
                 </select>
-                <input type="text" placeholder="CURP" {...register("CURP", { required: true })} />
-                <select name="escolaridad" {...register("escolaridad", { required: true })}>
+                <label className="label-form">CURP</label>
+                <input className="input-form" type="text" placeholder="CURP" {...register("CURP", { required: true })} />
+                <label className="label-form">Escolaridad</label>
+                <select className="input-form" name="escolaridad" {...register("escolaridad", { required: true })}>
                     <option value="Primaria">Primaria</option>
                     <option value="Secundaria">Secundaria</option>
                     <option value="Bachillerato">Bachillerato</option>
                     <option value="Divorciado">Universidad</option>
                 </select>
-                <input type="text" placeholder="Colonia" {...register("colonia", { required: true })} />
-                <input type="text" placeholder="Calle" {...register("calle", { required: true })} />
-                <input type="number" placeholder="Número" {...register("numero_exterior", { required: true })} />
-                <textarea placeholder="Entre calles o referencia" {...register("referencia", { required: true })}></textarea>
-                <input type="number" placeholder="CP" {...register("CP", { required: true })} />
-                <input type="text" placeholder="Telefono" {...register("telefono", { required: true })} />
-                <select name="derecho_habiencia" {...register("derecho_habiencia", { required: true })}>
+                <label className="label-form">Colonia</label>
+                <input className="input-form" type="text" placeholder="Colonia" {...register("colonia", { required: true })} />
+                <label className="label-form">Calle</label>
+                <input className="input-form" type="text" placeholder="Calle" {...register("calle", { required: true })} />
+                <label className="label-form">Número exterior</label>
+                <input className="input-form" type="number" placeholder="Número" {...register("numero_exterior", { required: true })} />
+                <label className="label-form">Entre que calles o referencia</label>
+                <textarea className="input-form" placeholder="Entre calles o referencia" {...register("referencia", { required: true })}></textarea>
+                <label className="label-form">CP</label>
+                <input className="input-form" type="number" placeholder="CP" {...register("CP", { required: true })} />
+                <label className="label-form">Teléfono</label>
+                <input className="input-form" type="text" placeholder="Telefono" {...register("telefono", { required: true })} />
+                <label className="label-form">Derechohabiencia</label>
+                <select className="input-form" name="derecho_habiencia" {...register("derecho_habiencia", { required: true })}>
                     <option value="IMSS">IMSS</option>
                     <option value="ISSSTE">ISSSTE</option>
                     <option value="PEMEX">PEMEX</option>
@@ -87,10 +102,12 @@ export function RegisterPaciente() {
                     <option value="SEDMAR">SEDMAR</option>
                     <option value="SSA/SESVER">SSA/SESVER</option>
                 </select>
-                <input type="text" placeholder="Unidad de salud" {...register("unidad_salud", { required: true })} />
-                <input type="date" placeholder="Ultima visita con su medico" {...register("ultima_visita_medico", { required: true })} />
+                <label className="label-form">Unidad de salud</label>
+                <input className="input-form" type="text" placeholder="Unidad de salud" {...register("unidad_salud", { required: true })} />
+                <label className="label-form">Última visita al médico</label>
+                <input className="input-form" type="date" placeholder="Ultima visita con su medico" {...register("ultima_visita_medico", { required: true })} />
 
-                <label>Es beneficiario de algún programa de gobierno</label>
+                <label className="label-form">Es beneficiario de algún programa de gobierno</label>
                 <label>Federal</label>
                 <label for="federal_si">Si
                     <input type="radio" id="federal_si" name="federal_option" value={true} {...register("programa_gobierno_federal", { required: true })} onChange={handleRadioFederalChange} />
@@ -101,7 +118,7 @@ export function RegisterPaciente() {
                 {showCualFederal && (
                     <div>
                         <label>¿Cuál</label>
-                        <input type="text" placeholder="Nombre del programa federal" {...register("cual_programa_federal")} />
+                        <input className="input-form" type="text" placeholder="Nombre del programa federal" {...register("cual_programa_federal")} />
                     </div>
                 )}
                 <label>Estatal</label>
@@ -114,7 +131,7 @@ export function RegisterPaciente() {
                 {showCualEstatal && (
                     <div>
                         <label>¿Cuál</label>
-                        <input type="text" placeholder="Nombre del programa estatal" {...register("cual_programa_estatal")} />
+                        <input className="input-form" type="text" placeholder="Nombre del programa estatal" {...register("cual_programa_estatal")} />
                     </div>
                 )}
                 <label>Municipal</label>
@@ -127,11 +144,11 @@ export function RegisterPaciente() {
                 {showCualMunicipal && (
                     <div>
                         <label>¿Cuál</label>
-                        <input type="text" placeholder="Nombre del programa municipal" {...register("cual_programa_municipal")} />
+                        <input className="input-form" type="text" placeholder="Nombre del programa municipal" {...register("cual_programa_municipal")} />
                     </div>
                 )}
-                <input type="number" placeholder="Cuantas personas vive con usted" {...register("numero_personas_vive", { required: true })} />
-                <button>Registrar</button>
+                <input className="input-form" type="number" placeholder="Cuantas personas vive con usted" {...register("numero_personas_vive", { required: true })} />
+                <button className="button-form">Registrar</button>
             </form>
         </>
     )
